2207c6fd50bb7c7ce0f80c5a2a3de724758c2def
Eduardo Matos <144041611+Eduardo-Matos-Cin@users.noreply.github.com>
2024-02-18 19:59:53 -0300

    Update main.py
    Adição da interface grafica

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pandas as pd
+import tkinter as tk
+from tkinter import ttk
 
 def extrair_min(Q):
     min_idx = 0
@@ -11,37 +15,94 @@ def extrair_min(Q):
 
     return Q.pop(min_idx)
 
-def prim(G, inicio):
+def prim_animation(G, inicio, entry_pesos):
     π = {v: None for v in G}
     Q = [(0, inicio, None)]
     S = set()
 
-    G_visual = nx.Graph()  # Visualização do grafo função
+    fig, ax = plt.subplots(figsize=(8, 8))
+    G_visual = nx.Graph()
 
-    while Q:
-        peso, v, pai = extrair_min(Q)
+    def update(frame):
+        nonlocal G_visual
+        nonlocal S
 
-        if v not in S:
-            S.add(v)
+        if Q:
+            peso, v, pai = extrair_min(Q)
 
-            if pai is not None:
-                print(f"{pai} -> {v} {peso}")
-                G_visual.add_edge(pai, v, weight=peso)
+            if v not in S:
+                S.add(v)
 
-            if v in G:
-                for u, peso_uv in G[v].items():
-                    if u not in S and (π[u] is None or peso_uv < G[v][π[u]]):
-                        Q.append((peso_uv, u, v))
-                        π[u] = v
+                if pai is not None:
+                    G_visual.add_edge(pai, v, weight=peso, color='blue', font_size=5)
+                    entry_pesos.insert(tk.END, f"{pai} -> {v} {peso}\n")
 
-    # funções para ver o grafo
-    pos = nx.spring_layout(G_visual)
-    labels = nx.get_edge_attributes(G_visual, 'weight')
-    nx.draw(G_visual, pos, with_labels=True, font_weight='bold')
-    nx.draw_networkx_edge_labels(G_visual, pos, edge_labels=labels)
-    plt.show()
+                if v in G:
+                    for u, peso_uv in G[v].items():
+                        if u not in S and (π[u] is None or peso_uv < G[v][π[u]]):
+                            Q.append((peso_uv, u, v))
+                            π[u] = v
 
-# Ler dados da planilha
+            pos = nx.spring_layout(G_visual, seed=43, k=1.5)
+            labels = nx.get_edge_attributes(G_visual, 'weight')
+            node_colors = ['blue' if node == inicio else 'red' if node == v else 'lightblue' for node in G_visual.nodes()]
+            edge_colors = ['blue' if (u, v) in G_visual.edges(data=False) and (v, u) in G_visual.edges(data=False) else 'gray'
+                           for u, v in G_visual.edges()]
+
+            ax.clear()
+            nx.draw(G_visual, pos, with_labels=True, font_weight='bold', node_color=node_colors, edge_color='black',
+                    node_size=400, font_size=6)
+            nx.draw_networkx_edge_labels(G_visual, pos, edge_labels=labels, font_size=8)
+
+    ani = FuncAnimation(fig, update, frames=range(len(G)), interval=400)
+
+    return fig, ani
+
+def iniciar_animacao(grafo):
+    def iniciar_animacao_callback():
+        vertice_inicio = combo_cidades.get()
+        fig, ani = prim_animation(grafo, vertice_inicio, entry_pesos)
+
+        window_animacao = tk.Toplevel(root)
+        window_animacao.title("Grafo")
+        window_animacao.geometry("800x600")
+
+        canvas = FigureCanvasTkAgg(fig, master=window_animacao)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+        ani.event_source.start()
+        window_animacao.protocol("WM_DELETE_WINDOW", lambda: on_closing(window_animacao, ani))
+
+    root = tk.Tk()
+    root.title("Seleção de Cidade Inicial")
+    root.geometry("400x300")
+
+    frame = ttk.Frame(root)
+    frame.pack(expand=True, pady=20)
+
+    label_cidade = ttk.Label(frame, text="Selecione a Cidade Inicial:")
+    label_cidade.pack(pady=10)
+
+    cidades = list(grafo.keys())
+
+    combo_cidades = ttk.Combobox(frame, values=cidades)
+    combo_cidades.set(cidades[0])
+    combo_cidades.pack(pady=10)
+
+    btn_iniciar_animacao = ttk.Button(frame, text="Iniciar", command=iniciar_animacao_callback)
+    btn_iniciar_animacao.pack()
+
+    entry_pesos = tk.Text(root, width=30, height=15)
+    entry_pesos.pack(pady=10)
+
+    def on_closing(window, animation):
+        plt.close(animation._fig)
+        window.destroy()
+
+    root.mainloop()
+
+# Ler os  dados da planilha
 df = pd.read_excel('distancias.xlsx')
 
 # Criar o grafo a partir dos dados da planilha
@@ -58,10 +119,6 @@ for index, row in df.iterrows():
         grafo[destino] = {}
 
     grafo[origem][destino] = distancia
-    grafo[destino][origem] = distancia  # Grafo não-direcionado
+    grafo[destino][origem] = distancia
 
-# Mostrar cidades no grafo
-print("Cidades no grafo:", list(grafo.keys()))
-
-vertice_inicio = input("Vértice inicial: ")
-prim(grafo, vertice_inicio)
+iniciar_animacao(grafo)
